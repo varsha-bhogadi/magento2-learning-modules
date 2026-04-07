f0a8e95c8a1b3fd8126f21e1166e69bfef22a5b4
root <varshabhogadi@gmail.com>
2026-04-07 08:54:25 +0000

    Custom router now works for /custom-page URL

--- app/code/Vendor/Routing/Controller/Router.php
+++ app/code/Vendor/Routing/Controller/Router.php
@@ -3,30 +3,16 @@ namespace Vendor\Routing\Controller;
 
 use Magento\Framework\App\RouterInterface;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\ActionFactory;
 
 class Router implements RouterInterface
 {
-    protected $actionFactory;
-
-    public function __construct(ActionFactory $actionFactory)
-    {
-        $this->actionFactory = $actionFactory;
-    }
-
     public function match(RequestInterface $request)
     {
         $identifier = trim($request->getPathInfo(), '/');
 
-        if ($identifier === 'customroute/index/index') {
-
-            $request->setModuleName('customroute')
-                ->setControllerName('index')
-                ->setActionName('index');
-
-            return $this->actionFactory->create(
-                \Vendor\Routing\Controller\Index\Index::class
-            );
+        if ($identifier === 'custom-page') {
+            // Rewrite to standard route
+            $request->setPathInfo('/customroute/index/index');
         }
 
         return null;
