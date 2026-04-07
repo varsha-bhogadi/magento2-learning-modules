8847f9bd56b10b2490a5698503f6bb6fe74e1bbc
root <varshabhogadi@gmail.com>
2026-04-07 09:49:18 +0000

    Add Magento headers and strict types to Routing module

--- app/code/Vendor/Routing/registration.php
+++ app/code/Vendor/Routing/registration.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
